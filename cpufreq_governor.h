eb192fba3526808fc403a72862eec2cc71034a3e
ZaneZam <cyxman@yahoo.com>
2017-07-11 10:27:49 +0200

    A New Beginning: bring in big.LITTLE Edition (bLE) development version
    this version is meant to use with kernel version 4.4.x
    
    working features in this first version:
    
    - zzmoove freq scaling method (without limit functionality)
    - proportional scaling (all modes)
    - fast scaling up/down with afs (fully functional)

--- cpufreq_governor.h
+++ cpufreq_governor.h
@@ -126,6 +126,7 @@ static void *get_cpu_dbs_info_s(int cpu)				\
  * cdbs: common dbs
  * od_*: On-demand governor
  * cs_*: Conservative governor
+ * zz_*: ZZMoove governor
  */
 
 /* Common to all CPUs of a policy */
@@ -172,6 +173,13 @@ struct cs_cpu_dbs_info_s {
 	unsigned int requested_freq;
 };
 
+struct zz_cpu_dbs_info_s {
+	struct cpu_dbs_info cdbs;
+	struct cpufreq_frequency_table *freq_table;
+	unsigned int down_skip;
+	unsigned int requested_freq;
+};
+
 /* Per policy Governors sysfs tunables */
 struct od_dbs_tuners {
 	unsigned int ignore_nice_load;
@@ -191,12 +199,29 @@ struct cs_dbs_tuners {
 	unsigned int freq_step;
 };
 
+struct zz_dbs_tuners {
+	unsigned int ignore_nice_load;
+	unsigned int sampling_rate;
+	unsigned int sampling_down_factor;
+	unsigned int up_threshold;
+	unsigned int down_threshold;
+	unsigned int smooth_up;
+	unsigned int scaling_proportional;
+	unsigned int fast_scaling_up;
+	unsigned int fast_scaling_down;
+	unsigned int afs_threshold1;
+	unsigned int afs_threshold2;
+	unsigned int afs_threshold3;
+	unsigned int afs_threshold4;
+};
+
 /* Common Governor data across policies */
 struct dbs_data;
 struct common_dbs_data {
 	/* Common across governors */
 	#define GOV_ONDEMAND		0
 	#define GOV_CONSERVATIVE	1
+	#define GOV_ZZMOOVE		2
 	int governor;
 	struct attribute_group *attr_group_gov_sys; /* one governor - system */
 	struct attribute_group *attr_group_gov_pol; /* one governor - policy */
@@ -229,6 +254,19 @@ struct common_dbs_data {
 struct dbs_data {
 	struct common_dbs_data *cdata;
 	unsigned int min_sampling_rate;
+	struct cpufreq_frequency_table *freq_table;
+	bool freq_table_desc;
+	bool scaling_eval_done;
+	unsigned int freq_table_size;
+	unsigned int pol_min;
+	unsigned int pol_max;
+	unsigned int min_scaling_freq;
+	unsigned int limit_table_start;
+	unsigned int limit_table_end;
+	unsigned int max_scaling_freq_hard;
+	unsigned int max_scaling_freq_soft;
+	unsigned int scaling_mode_up;
+	unsigned int scaling_mode_down;
 	int usage_count;
 	void *tuners;
 };
